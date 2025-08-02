2ecbf6e21a95b66efe19b012641f71a36b391993
cristiadu <cristiadu@gmail.com>
2025-08-02 14:39:16 -0700

    initial drawing of tracks

--- tracks_menu.gd
+++ tracks_menu.gd
@@ -3,10 +3,7 @@ extends VBoxContainer
 @onready var track_piece_scene = preload("res://track_piece.tscn")
 @onready var grid_container: GridContainer = $ScrollContainer/MarginContainer/GridContainer
 
-const INITIAL_TRACK_PIECE_COUNT = 10
-
-func _ready():
-  generate_track_piece_options()
+const INITIAL_TRACK_PIECE_COUNT = 15
 
 func generate_track_piece_options():
   # Call track piece scene to generate track pieces in a grid layout
